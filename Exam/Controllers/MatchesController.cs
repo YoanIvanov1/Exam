c7a917a48a0b71c5b33719dc2c4096dfc333470f
Yoan <yoan.iv00@gmail.com>
2026-03-12 06:46:24 +0200

    Add Match model, MatchesController, and Matches Index view

---
+++ Exam/Controllers/MatchesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exam.Controllers
+{
+    public class MatchesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var matches = new List<Models.Match>
+            {
+                new Models.Match { Id = 1, TeamA = "LR", TeamB = "G2", Date = new DateTime(2024, 7, 15), Location = "Stadium A" },
+                new Models.Match { Id = 2, TeamA = "KC", TeamB = "MKOI", Date = new DateTime(2024, 7, 16), Location = "Stadium B" },
+                new Models.Match { Id = 3, TeamA = "LR", TeamB = "KC", Date = new DateTime(2024, 7, 17), Location = "Stadium C" },
+                new Models.Match { Id = 4, TeamA = "G2", TeamB = "MKOI", Date = new DateTime(2024, 7, 18), Location = "Stadium D" }
+            };
+
+            return View(matches);
+        }
+    }
+}
---
+++ Exam/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿namespace Exam.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+
+        public string TeamA { get; set; }
+
+        public string TeamB { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Location { get; set; }
+    }
+}
