35b470496172f5ad5735e045ab3d08276db4645c
Yoan <yoan.iv00@gmail.com>
2026-03-12 08:28:14 +0200

    Modified and Updated the UI

--- Exam/Controllers/HomeController.cs
+++ Exam/Controllers/HomeController.cs
@@ -6,20 +6,17 @@ namespace Exam.Controllers
 {
     public class HomeController : Controller
     {
+        private static List<Match> matches = new List<Models.Match>
+        {
+            new Models.Match { Id = 1, TeamA = "LR", TeamB = "G2", Date = new DateTime(2024, 7, 15), Location = "Stadium A" },
+            new Models.Match { Id = 2, TeamA = "KC", TeamB = "MKOI", Date = new DateTime(2024, 7, 16), Location = "Stadium B" },
+            new Models.Match { Id = 3, TeamA = "LR", TeamB = "KC", Date = new DateTime(2024, 7, 17), Location = "Stadium C" },
+            new Models.Match { Id = 4, TeamA = "G2", TeamB = "MKOI", Date = new DateTime(2024, 7, 18), Location = "Stadium D" }
+        };
+
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(matches);
         }
     }
 }
