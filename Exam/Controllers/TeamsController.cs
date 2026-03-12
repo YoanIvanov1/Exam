1253cf12364a238398a68c4301aa469b105cd316
Yoan <yoan.iv00@gmail.com>
2026-03-12 06:22:58 +0200

    Add Team model, TeamsController, and Teams Index view

---
+++ Exam/Controllers/TeamsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Exam.Controllers
+{
+    public class TeamsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var teams = new List<Models.Team>
+            {
+                new Models.Team { Id = 1, Name = "LR", Region = "Europe", Ranking = 1 },
+                new Models.Team { Id = 2, Name = "G2", Region = "Europe", Ranking = 2 },
+                new Models.Team { Id = 3, Name = "KC", Region = "Europe", Ranking = 3 },
+                new Models.Team { Id = 4, Name = "MKOI", Region = "Europe", Ranking = 4 }
+            };
+
+            return View(teams);
+        }
+    }
+}
