9372d41a82bfd77e923120d5b07b75fc8d48c15b
DK\yoan <yoan@sitecore.net>
2026-03-16 11:46:45 +0200

    Added AccountController, Profile view to display profile information and bets made by the logged user, added a Logout option to the Profile view page

---
+++ Exam/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Exam.Data;
+using Exam.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<User> _userManager;
+    private readonly UserDbContext _context;
+    private readonly SignInManager<User> _signInManager;
+
+    public AccountController(
+        UserManager<User> userManager,
+        UserDbContext context,
+        SignInManager<User> signInManager)
+    {
+        _userManager = userManager;
+        _context = context;
+        _signInManager = signInManager;
+    }
+
+    public async Task<IActionResult> Profile()
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        if (user == null)
+            return RedirectToPage("/Account/Login", new { area = "Identity" });
+
+        var bets = await _context.Bets
+            .Where(b => b.UserId == user.Id)
+            .OrderByDescending(b => b.PlacedAt)
+            .ToListAsync();
+
+        var model = new ProfileViewModel
+        {
+            Email = user.Email,
+            WalletBalance = user.WalletBalance,
+            Bets = bets
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Logout()
+    {
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+}
---
+++ Exam/Models/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Exam.Models
+{
+    public class ProfileViewModel
+    {
+        public string Email { get; set; }
+
+        public int WalletBalance { get; set; }
+
+        public List<Bet> Bets { get; set; }
+    }
+}
